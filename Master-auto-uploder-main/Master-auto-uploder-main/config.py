b2a424cf525e52c93896963cbb735a175f4d5f1f
gparwal5-hash <gparwal5@gmail.com>
2026-09-24 23:02:13 +0530

    Update configuration values in config.py

--- Master-auto-uploder-main/Master-auto-uploder-main/config.py
+++ Master-auto-uploder-main/Master-auto-uploder-main/config.py
@@ -3,12 +3,12 @@ import os
 ##Code Written By ꧁༒☬𝗦AHIL DEVLOPER☬༒꧂
 
 class Config(object):
-    BOT_TOKEN = "8842602749:AAECqZU_dC4mdqrymIb7cmA8HZqylD144tM"
+    BOT_TOKEN = ""
     DB_NAME = "master_txt_to_video_auto_uploader"
-    API_ID = "28221704"
-    API_HASH = "a0163f47bc5262a3106b7858720a09fb"
-    ADMIN_ID = 8321801403
+    API_ID = "23878955"
+    API_HASH = "07637a6c4c4566dc3923fa29d6c84b67"
+    ADMIN_ID = 5445688589
     DB_URL = "mongodb+srv://sg4828990_db_user:<YOUR_MONGODB_PASSWORD>@cluster0.2brxvvv.mongodb.net/master_txt_to_video_auto_uploader?retryWrites=true&w=majority&appName=Cluster0"
-    LOG_CHANNEL = "-1004374834303" # Your Log Channel ID (Bot ko ADMIN BNAYE)
+    LOG_CHANNEL = "-1003113891183" # Your Log Channel ID (Bot ko ADMIN BNAYE)
     USERLINK = "http://t.me/masterautouploader_bot"
     
